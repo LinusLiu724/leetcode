38fdb7ec816b1c2f9f397e84b73f15f5fcb1fe2d
liuz4 <liuz4@wangsu.com>
2020-07-06 11:33:04 +0800

    通配符匹配

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # leetcode
+
+### 1、**`链表中倒数第k个节点`** getKthFromEnd 
+
+### 	**`解法：快慢指针`**：https://www.lbbniu.com/6688.html
+
+### 2、**`通配符匹配`** isMatch 
+
+### 	**`解法：字符串动态规划`**：https://leetcode-cn.com/problems/wildcard-matching/solution/tong-pei-fu-pi-pei-by-leetcode-solution/
+
+Webrtc国内源码镜像地址：
+https://webrtc.org.cn/mirror/
+
+ Mingw-w64
+https://code.visualstudio.com/docs/cpp/config-mingw
+https://sourceforge.net/projects/mingw-w64/postdownload
+
--- isMatch.cpp
+++ isMatch.cpp
@@ -1,6 +1,10 @@
 /**
  * 通配符匹配
  * 给定一个字符串 (s) 和一个字符模式 (p) ，实现一个支持 '?' 和 '*' 的通配符匹配。
+ * 状态转移方程：
+ * 1、s为空，p 为空：dp[0][0] = ture;
+ * 2、s为空，p 不为空，恒为假，false
+ * 3、
  */
 
 #include <iostream>
@@ -10,7 +14,37 @@ using namespace std;
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        s.size();
-        p.size();
+        int s_lens = s.size();
+        int p_lens = p.size();
+        int i, j;
+
+        vector<vector<boolean>> dp(s_lens + 1, vector<boolean>(p_lens + 1));
+        dp[0][0] = true;
+
+        /* p 开头为多个*号的情况 */
+        for (i = 1; i <= p_lens, i++) {
+            if (p[i - 1] = '*') {
+                dp[0][i] = true;
+            } else {
+                break;
+            }
+        }
+
+        for (i = 1; i <= s_lens, i++) {
+            for (j = 1; j < p_lens, j++) {
+                if (p[j - 1] == '?') {
+
+                } else if (p[j - 1] == '*') {
+
+                } else if (s[i - 1] == (p[j - 1]) {
+
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+
+
+
     }
-};
+};b
